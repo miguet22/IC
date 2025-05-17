a0ff93ac9e63e48a3abf66e1e69747487e42f016
miguet22 <joamigue22@gmail.com>
2025-05-17 16:35:28 -0300

    cambios22

---
+++ pruebas/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    resultado = 5 * 5
+    return f"El resultado es: {resultado}"
+
+if __name__ == "__main__":
+    app.run()
+
